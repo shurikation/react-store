4b50482ef9020a3f01a93b32d140459ca62149c8
shurikation <shurikation@gmail.com>
2021-06-03 16:30:13 +0400

    feat: add menu-item, directory components and homepage to separate folders

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './homepage.styles.scss';
+
+import Directory from '../../components/directory/directory.component';
+
+
+const HomePage = () => (
+	<section className="homepage">
+		<Directory />
+	</section>
+)
+
+export default HomePage;
