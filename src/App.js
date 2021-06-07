45939642bc9ea4ad545ed2084afa0c2169df7fa8
shurikation <shurikation@gmail.com>
2021-06-07 22:54:00 +0400

    feat: [TEMP] add some components for testing routing

--- src/App.js
+++ src/App.js
@@ -2,23 +2,40 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
-import HomePage from '../src/pages/homepage/homepage.component.jsx';
+// import HomePage from '../src/pages/homepage/homepage.component.jsx';
 
-const HatsPage = () => (
+const HomePage = () => {
+	return (
 	<div>
-		<h1>HATS PAGE</h1>
+		<h1>HOME PAGE</h1>
 	</div>
-)
+	);
+};
+
+const TopicsList = () => {
+	return (
+		<div>
+			<h1>TOPIC PAGE</h1>
+		</div>
+	)
+}
+
+const TopicsDetail = () => {
+	return (
+		<div>
+			<h1>TOPIC DETAIL</h1>
+		</div>
+	)
+}
 
 function App() {
 	return (
 		<div>
-			<Switch>
-				<Route exact path='/' component={HomePage} />
-				<Route path='/hats' component={HatsPage} />
-			</Switch>
+			<Route exact path='/' component={HomePage} />
+			<Route exact path='/topics' component={TopicsList} />
+			<Route path='/topics/:topicId' component={TopicsDetail} />
 		</div>
-	);
+	)
 }
 
 export default App;
