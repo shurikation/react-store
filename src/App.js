f4af094a12f758b254f24054291c3efb8bdb807a
shurikation <shurikation@gmail.com>
2021-06-21 21:13:57 +0400

    feat: [TEMP] add dinamic routing links for example components

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import './App.css';
 
 // import HomePage from '../src/pages/homepage/homepage.component.jsx';
 
-const HomePage = () => {
+const HomePage = (props) => {
+	console.log(props)
 	return (
-	<div>
-		<h1>HOME PAGE</h1>
-	</div>
+		<div>
+			<Link to='/topics'>Topics</Link> 
+			<button onClick={() => props.history.push('/topics')}>Topics (same)</button>
+			<h1>HOME PAGE</h1>
+		</div>
 	);
 };
 
-const TopicsList = () => {
+const TopicsList = (props) => {
+	console.log(props)
 	return (
 		<div>
 			<h1>TOPIC PAGE</h1>
+			<Link to={`${props.match.url}/13`}>TO 13 TOPIC</Link>
+			<Link to={`${props.match.url}/17`}>TO 17 TOPIC</Link>
+			<Link to={`${props.match.url}/23`}>TO 23 TOPIC</Link>
 		</div>
 	)
 }
 
-const TopicsDetail = () => {
+const TopicsDetail = (props) => {
+console.log(props); 
 	return (
 		<div>
-			<h1>TOPIC DETAIL</h1>
+			<h1>TOPIC DETAIL PAGE {props.match.params.topicId}</h1>
 		</div>
 	)
 }
@@ -31,8 +39,8 @@ const TopicsDetail = () => {
 function App() {
 	return (
 		<div>
-			<Route exact path='/' component={HomePage} />
-			<Route exact path='/topics' component={TopicsList} />
+			<Route path='/' component={HomePage} />
+			<Route exact path='/kek/kkkk/topics' component={TopicsList} />
 			<Route path='/topics/:topicId' component={TopicsDetail} />
 		</div>
 	)
