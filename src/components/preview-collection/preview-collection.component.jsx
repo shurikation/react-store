1e65b98159a5a07eef0735b367e66c66ea6ff71c
shurikation <shurikation@gmail.com>
2021-06-22 22:47:10 +0400

    feat: add collection-item component and styles

--- src/components/preview-collection/preview-collection.component.jsx
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./preview-collection.styles.scss";
+import CollectionItem from "../collection-item/collection-item.component";
 
 const PreviewCollection = ({ title, items }) => (
   <div className="collection-preview">
@@ -7,10 +8,11 @@ const PreviewCollection = ({ title, items }) => (
     <div className="preview">
       {items
         .filter((item, idx) => idx < 4)
-        .map((item) => (
-          <div key={item.id}>{item.name}</div>
+        .map(({id, ...otherItemProps}) => (
+          <CollectionItem key={id} {...otherItemProps}/>
         ))}
     </div>
   </div>
 );
+
 export default PreviewCollection;
